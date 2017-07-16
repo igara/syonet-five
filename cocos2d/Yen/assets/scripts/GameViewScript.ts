0acb3e50c2ec1965b4995cb5a2edbfa2145f386e
Igarashi Syo <igara1119@gmail.com>
2017-07-16 23:59:18 +0900

    5000兆円降ってきた

--- cocos2d/Yen/assets/scripts/GameViewScript.ts
+++ cocos2d/Yen/assets/scripts/GameViewScript.ts
@@ -4,11 +4,6 @@ const {ccclass, property} = cc._decorator;
 const physicsManager = cc.director.getPhysicsManager();
 physicsManager.enabled = true;
 
-// 衝突を扱う
-const manager = cc.director.getCollisionManager();
-// デフォルトfalseになっているのをtrueにして衝突検知をOnにする
-manager.enabled = true;
-
 /**
  * ゲーム箇所のComponent
  * @extends {cc.Component}
@@ -29,6 +24,102 @@ export default class GameViewScript extends cc.Component {
      */
     @property(cc.Label)
     playerCount: cc.Label;
+
+    /**
+     * 左の価格
+     * @type {cc.Sprite} leftYen
+     */
+    @property(cc.Sprite)
+    leftYen: cc.Sprite;
+
+    /**
+     * 右の価格
+     * @type {cc.Sprite} rightYen
+     */
+    @property(cc.Sprite)
+    rightYen: cc.Sprite;
+
+    /**
+     * 0
+     * @type {cc.Sprite} zero
+     */
+    @property(cc.Sprite)
+    zero: cc.Sprite;
+
+    /**
+     * 1
+     * @type {cc.Sprite} one
+     */
+    @property(cc.Sprite)
+    one: cc.Sprite;
+
+    /**
+     * 2
+     * @type {cc.Sprite} two
+     */
+    @property(cc.Sprite)
+    two: cc.Sprite;
+
+    /**
+     * 3
+     * @type {cc.Sprite} three
+     */
+    @property(cc.Sprite)
+    three: cc.Sprite;
+
+    /**
+     * 4
+     * @type {cc.Sprite} four
+     */
+    @property(cc.Sprite)
+    four: cc.Sprite;
+
+    /**
+     * 5
+     * @type {cc.Sprite} five
+     */
+    @property(cc.Sprite)
+    five: cc.Sprite;
+
+    /**
+     * 6
+     * @type {cc.Sprite} six
+     */
+    @property(cc.Sprite)
+    six: cc.Sprite;
+
+    /**
+     * 7
+     * @type {cc.Sprite} seven
+     */
+    @property(cc.Sprite)
+    seven: cc.Sprite;
+
+    /**
+     * 8
+     * @type {cc.Sprite} eight
+     */
+    @property(cc.Sprite)
+    eight: cc.Sprite;
+
+    /**
+     * 9
+     * @type {cc.Sprite} nine
+     */
+    @property(cc.Sprite)
+    nine: cc.Sprite;
+
+    /**
+     * 5000兆円
+     * @type {cc.Node} tyoYen
+     */
+    @property(cc.Node)
+    tyoYen: cc.Node;
+
+    /**
+     * @type {Number: cc.Sprite.spriteFrame} 数字のSpriteFrame
+     */
+    numSpriteFrame = {};
 
     /**
      * ↑ボタン
@@ -80,6 +171,18 @@ export default class GameViewScript extends cc.Component {
      */
     onLoad() {
         this.playerCount.string = GlobalVals.PlayerCount;
+        this.numSpriteFrame = {
+            0: this.zero.spriteFrame,
+            1: this.one.spriteFrame,
+            2: this.two.spriteFrame,
+            3: this.three.spriteFrame,
+            4: this.four.spriteFrame,
+            5: this.five.spriteFrame,
+            6: this.six.spriteFrame,
+            7: this.seven.spriteFrame,
+            8: this.eight.spriteFrame,
+            9: this.nine.spriteFrame,
+        };
     }
 
     /**
@@ -102,6 +205,31 @@ export default class GameViewScript extends cc.Component {
             GlobalVals.GameStatus = GlobalVals.GameStatusGroup.start;
             this.player.x = 0;
             this.player.y = 110;
+        }
+        if (GlobalVals.GameStatus !== GlobalVals.GameStatusGroup.dead) {
+            if (GlobalVals.TotalYen > 0) {
+                GlobalVals.TotalYen -= 1;
+                GlobalVals.RightYen += 1;
+                if (GlobalVals.RightYen > 9) {
+                    GlobalVals.RightYen = 0;
+                    this.rightYen.spriteFrame = this.numSpriteFrame[GlobalVals.RightYen];
+                    GlobalVals.LeftYen += 1;
+                    if (GlobalVals.LeftYen > 9) {
+                        GlobalVals.LeftYen = 0;
+                        this.leftYen.spriteFrame = this.numSpriteFrame[GlobalVals.LeftYen];
+                        GlobalVals.PlayerCount += 1;
+                    } else {
+                        this.leftYen.spriteFrame = this.numSpriteFrame[GlobalVals.LeftYen];
+                    }
+                } else {
+                    this.rightYen.spriteFrame = this.numSpriteFrame[GlobalVals.RightYen];
+                }
+            }
+        }
+        if (GlobalVals.isTyoYenBlockCol === true) {
+            this.tyoYen.y = 300;
+            this.tyoYen.x = 0;
+            GlobalVals.isTyoYenBlockCol = false;
         }
     }
 
@@ -128,31 +256,6 @@ export default class GameViewScript extends cc.Component {
      * @see {cc.Component.onDestroy}
      */
     onDestroy() {
-    }
-
-    /**
-     * 衝突開始した時に呼び出されるメソッド
-     * @param {{}} other 衝突したNode
-     * @param {{}} self 自身のNode
-     */
-    onCollisionEnter(other, self) {
-    }
-
-    /**
-     * 衝突中に呼び出されるメソッド
-     * @param {{}} other 衝突したNode
-     * @param {{}} self 自身のNode
-     */
-    onCollisionStay(other, self) {
-    }
-
-    /**
-     * 衝突が解除される時に呼び出されるメソッド
-     * @param {{}} other 衝突したNode
-     * @param {{}} self 自身のNode
-     */
-    onCollisionExit(other, self) {
-        GlobalVals.GameStatus = GlobalVals.GameStatusGroup.dead;
     }
 
     /**
--- cocos2d/Yen/assets/scripts/GlobalsScript.ts
+++ cocos2d/Yen/assets/scripts/GlobalsScript.ts
@@ -1,7 +1,7 @@
 // declare global variable "GlobalVals"
 window.GlobalVals = {
     // ゲームの状態を扱う
-    GameStatus: 0,
+    GameStatus: 1,
     GameStatusGroup: {
         dead: 0,
         start: 1,
@@ -15,4 +15,8 @@ window.GlobalVals = {
     RightYen: 0,
     // 価格の合計
     TotalYen: 0,
+    // 5000兆円
+    TyoYen: 5000000000000000,
+    // 5000兆円出てくるブロックとの衝突あったとき一時的にtrue
+    isTyoYenBlockCol: false,
 };
